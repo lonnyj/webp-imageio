8c1f50c3401bc02b055ce81a02f67ba5e02f3b73
Lonny <ljacobso@nyx.net>
2014-10-08 13:53:39 -0600

    Added more tests of various compression options.

--- java/src/test/java/com/luciad/imageio/webp/WebPWriterTest.java
+++ java/src/test/java/com/luciad/imageio/webp/WebPWriterTest.java
@@ -22,60 +22,37 @@ import org.testng.annotations.Test;
  * <code>WebPWriterTest</code> unit tests {@link WebPWriter}.
  */
 public class WebPWriterTest {
+   /** The "lossy" compression type. */
+   private static final String LOSSY_COMPRESSION_TYPE = "Lossy";
+
+   /** The "lossless" compression type. */
+   private static final String LOSSLESS_COMPRESSION_TYPE = "Lossless";
 
    /**
-    * Test method for
-    * {@link com.luciad.imageio.webp.WebPWriter#write(javax.imageio.metadata.IIOMetadata, javax.imageio.IIOImage, javax.imageio.ImageWriteParam)}
-    * .
-    *
+    * Tests writing WebP images using
+    * {@link ImageIO#write(RenderedImage, String, java.io.OutputStream)}.
+    * 
+    * @param im
+    *           the image to write.
+    * @param outputName
+    *           the name of the file to write.
     * @throws IOException
+    *            if unable to write the image.
     */
-   @Test(dataProvider = "testImages", enabled = true)
-   public void testWriteIIOMetadataIIOImageImageWriteParam(final RenderedImage im,
-         final String outputName) throws IOException {
-      final String extension = outputName.substring(outputName.lastIndexOf(".") + 1);
-
-      // get writer
-      final ImageWriter imgWriter = ImageIO.getImageWritersByFormatName(extension).next();
-      final ImageWriteParam imgWriteParams = new WebPWriteParam(null);
-      final String compressionType = imgWriteParams.getCompressionTypes()[0];
-      imgWriteParams.setCompressionType(compressionType);
-      final float compressionQuality = 0.9f;
-      // 0~1
-      imgWriteParams.setCompressionQuality(compressionQuality);
-      final File parentDirectory = new File("target" + File.separator + "ImageWriter");
-      parentDirectory.mkdirs();
-      File file = new File(parentDirectory, outputName);
-   	final ImageOutputStream imageOutputStream = ImageIO
-   			.createImageOutputStream(file);
-      try {
-         imgWriter.setOutput(imageOutputStream);
-         imgWriter.write(null, new IIOImage(im, null, null), imgWriteParams);
-         final int length = (int) imageOutputStream.length();
-         assertTrue(length > 0);
-      } finally {
-         try {
-        	 imageOutputStream.close();
-         } catch (final IOException e) {
-         }
-      }
-   }
-
    @Test(dataProvider = "testImages", enabled = true)
    public void testImageIoWrite(final RenderedImage im, final String outputName) throws IOException {
       final ByteArrayOutputStream baoStream = new ByteArrayOutputStream();
       ImageIO.write(im, "webp", baoStream);
       assertTrue(baoStream.size() > 0);
-      final File parentDirectory = new File("target" + File.separator + "ImageIoWrite");
-      parentDirectory.mkdirs();
-      File file = new File(parentDirectory, outputName);
+      final String testName = "ImageIoWrite";
+      File file = createOutputFile(testName, outputName);
       final FileOutputStream foStream = new FileOutputStream(file);
       baoStream.writeTo(foStream);
       foStream.close();
    }
 
    /**
-    * The data provider for all test images.
+    * The data provider for {@link #testImageIoWrite}.
     *
     * @return the test {@link BufferedImage}s.
     * @throws IOException
@@ -90,5 +67,81 @@ public class WebPWriterTest {
             new Object[] { ImageIO.read(classLoader.getResource("3.png")), "3.webp" },
             new Object[] { ImageIO.read(classLoader.getResource("4.png")), "4.webp" },
             new Object[] { ImageIO.read(classLoader.getResource("5.png")), "5.webp" } };
+   }
+
+   /**
+    * Test method for
+    * {@link com.luciad.imageio.webp.WebPWriter#write(javax.imageio.metadata.IIOMetadata, javax.imageio.IIOImage, javax.imageio.ImageWriteParam)}
+    * .
+    *
+    * @throws IOException
+    */
+   @Test(dataProvider = "testImageOptions", enabled = true)
+   public void testWriteIIOMetadataIIOImageImageWriteParam(final RenderedImage im,
+         final String compressionType, final float compressionQuality, final String outputName)
+         throws IOException {
+      final String extension = outputName.substring(outputName.lastIndexOf(".") + 1);
+
+      // get writer
+      final ImageWriter imgWriter = ImageIO.getImageWritersByFormatName(extension).next();
+      final ImageWriteParam imgWriteParams = new WebPWriteParam(null);
+      imgWriteParams.setCompressionType(compressionType);
+      // 0~1
+      imgWriteParams.setCompressionQuality(compressionQuality);
+      final String testName = "ImageWriter";
+      File file = createOutputFile(testName, outputName);
+      final ImageOutputStream imageOutputStream = ImageIO.createImageOutputStream(file);
+      try {
+         imgWriter.setOutput(imageOutputStream);
+         imgWriter.write(null, new IIOImage(im, null, null), imgWriteParams);
+         final int length = (int) imageOutputStream.length();
+         assertTrue(length > 0);
+      } finally {
+         try {
+            imageOutputStream.close();
+         } catch (final IOException e) {
+         }
+      }
+   }
+
+   /**
+    * Creates the output file in the specified test directory.
+    * 
+    * @param testName
+    *           the name of the test to use as the output directory name.
+    * @param outputName
+    *           the name of the file to be created.
+    * @return the file destination.
+    */
+   private File createOutputFile(final String testName, final String outputName) {
+      final File parentDirectory = new File("target" + File.separator + testName);
+      parentDirectory.mkdirs();
+      File file = new File(parentDirectory, outputName);
+      return file;
+   }
+
+   /**
+    * The data provider for all test images.
+    *
+    * @return the test {@link BufferedImage}s.
+    * @throws IOException
+    *            if unable to read one of the test images.
+    */
+   @DataProvider
+   public Object[][] testImageOptions() throws IOException {
+      final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+      return new Object[][] {
+            new Object[] { ImageIO.read(classLoader.getResource("1.png")), LOSSY_COMPRESSION_TYPE, 0f, "1_lossy_0.webp" },
+            new Object[] { ImageIO.read(classLoader.getResource("1.png")), LOSSY_COMPRESSION_TYPE, 0.5f, "1_lossy_50.webp" },
+            new Object[] { ImageIO.read(classLoader.getResource("1.png")), LOSSY_COMPRESSION_TYPE, 0.9f, "1_lossy_90.webp" },
+            new Object[] { ImageIO.read(classLoader.getResource("1.png")), LOSSY_COMPRESSION_TYPE, 1f, "1_lossy_100.webp" },
+            new Object[] { ImageIO.read(classLoader.getResource("1.png")), LOSSLESS_COMPRESSION_TYPE, 0f, "1_lossless_0.webp" },
+            new Object[] { ImageIO.read(classLoader.getResource("1.png")), LOSSLESS_COMPRESSION_TYPE, 0.5f, "1_lossless_50.webp" },
+            new Object[] { ImageIO.read(classLoader.getResource("1.png")), LOSSLESS_COMPRESSION_TYPE, 0.9f, "1_lossless_90.webp" },
+            new Object[] { ImageIO.read(classLoader.getResource("1.png")), LOSSLESS_COMPRESSION_TYPE, 1f, "1_lossless_100.webp" },
+            new Object[] { ImageIO.read(classLoader.getResource("2.png")), LOSSY_COMPRESSION_TYPE, 0.9f, "2_lossy_90.webp" },
+            new Object[] { ImageIO.read(classLoader.getResource("3.png")), LOSSY_COMPRESSION_TYPE, 0.9f, "3_lossy_90.webp" },
+            new Object[] { ImageIO.read(classLoader.getResource("4.png")), LOSSY_COMPRESSION_TYPE, 0.9f, "4_lossy_90.webp" },
+            new Object[] { ImageIO.read(classLoader.getResource("5.png")), LOSSY_COMPRESSION_TYPE, 0.9f, "5_lossy_90.webp" } };
    }
 }
