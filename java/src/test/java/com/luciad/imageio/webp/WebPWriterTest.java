e84e7ea30f2d639f71f3f3c54469b7ba072fe96c
Lonny <gmljacobson@gmail.com>
2014-10-13 05:46:26 +0100

    Added more tests.

--- java/src/test/java/com/luciad/imageio/webp/WebPWriterTest.java
+++ java/src/test/java/com/luciad/imageio/webp/WebPWriterTest.java
@@ -14,6 +14,9 @@ import javax.imageio.ImageIO;
 import javax.imageio.ImageWriteParam;
 import javax.imageio.ImageWriter;
 import javax.imageio.stream.ImageOutputStream;
+import javax.media.jai.Interpolation;
+import javax.media.jai.RenderedOp;
+import javax.media.jai.operator.ScaleDescriptor;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -31,7 +34,7 @@ public class WebPWriterTest {
    /**
     * Tests writing WebP images using
     * {@link ImageIO#write(RenderedImage, String, java.io.OutputStream)}.
-    * 
+    *
     * @param im
     *           the image to write.
     * @param outputName
@@ -39,13 +42,13 @@ public class WebPWriterTest {
     * @throws IOException
     *            if unable to write the image.
     */
-   @Test(dataProvider = "testImages", enabled = true)
+   @Test(dataProvider = "createBasicImages", enabled = true)
    public void testImageIoWrite(final RenderedImage im, final String outputName) throws IOException {
       final ByteArrayOutputStream baoStream = new ByteArrayOutputStream();
       ImageIO.write(im, "webp", baoStream);
       assertTrue(baoStream.size() > 0);
-      final String testName = "ImageIoWrite";
-      File file = createOutputFile(testName, outputName);
+      final String testName = "ImageIo";
+      final File file = createOutputFile(testName, outputName);
       final FileOutputStream foStream = new FileOutputStream(file);
       baoStream.writeTo(foStream);
       foStream.close();
@@ -59,14 +62,12 @@ public class WebPWriterTest {
     *            if unable to read one of the test images.
     */
    @DataProvider
-   public Object[][] testImages() throws IOException {
-      final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-      return new Object[][] {
-            new Object[] { ImageIO.read(classLoader.getResource("1.png")), "1.webp" },
-            new Object[] { ImageIO.read(classLoader.getResource("2.png")), "2.webp" },
-            new Object[] { ImageIO.read(classLoader.getResource("3.png")), "3.webp" },
-            new Object[] { ImageIO.read(classLoader.getResource("4.png")), "4.webp" },
-            new Object[] { ImageIO.read(classLoader.getResource("5.png")), "5.webp" } };
+   public Object[][] createBasicImages() throws IOException {
+      return new Object[][] { new Object[] { loadImage("1.png"), "1.webp" },
+            new Object[] { loadImage("2.png"), "2.webp" },
+            new Object[] { loadImage("3.png"), "3.webp" },
+            new Object[] { loadImage("4.png"), "4.webp" },
+            new Object[] { loadImage("5.png"), "5.webp" } };
    }
 
    /**
@@ -76,8 +77,8 @@ public class WebPWriterTest {
     *
     * @throws IOException
     */
-   @Test(dataProvider = "testImageOptions", enabled = true)
-   public void testWriteWriter(final RenderedImage im, final String compressionType,
+   @Test(dataProvider = "createImagesWithCompressionOptions", enabled = true)
+   public void testImageWriterCompression(final RenderedImage im, final String compressionType,
          final float compressionQuality, final String outputName) throws IOException {
       final String extension = outputName.substring(outputName.lastIndexOf(".") + 1);
 
@@ -87,8 +88,8 @@ public class WebPWriterTest {
       imgWriteParams.setCompressionType(compressionType);
       // 0~1
       imgWriteParams.setCompressionQuality(compressionQuality);
-      final String testName = "ImageWriter";
-      File file = createOutputFile(testName, outputName);
+      final String testName = "CompressionOptions";
+      final File file = createOutputFile(testName, outputName);
       final ImageOutputStream imageOutputStream = ImageIO.createImageOutputStream(file);
       try {
          imgWriter.setOutput(imageOutputStream);
@@ -104,8 +105,103 @@ public class WebPWriterTest {
    }
 
    /**
+    * The data provider for
+    * {@link #testImageWriter(RenderedImage, String, float, String)}.
+    *
+    * @return the test images and options.
+    * @throws IOException
+    *            if unable to read one of the test images.
+    */
+   @DataProvider
+   public Object[][] createImagesWithCompressionOptions() throws IOException {
+      return new Object[][] {
+            new Object[] { loadImage("1.png"), LOSSY_COMPRESSION_TYPE, 0f, "1_lossy_0.webp" },
+            new Object[] { loadImage("1.png"), LOSSY_COMPRESSION_TYPE, 0.5f, "1_lossy_50.webp" },
+            new Object[] { loadImage("1.png"), LOSSY_COMPRESSION_TYPE, 0.9f, "1_lossy_90.webp" },
+            new Object[] { loadImage("1.png"), LOSSY_COMPRESSION_TYPE, 1f, "1_lossy_100.webp" },
+            new Object[] { loadImage("1.png"), LOSSLESS_COMPRESSION_TYPE, 0f, "1_lossless_0.webp" },
+            new Object[] { loadImage("1.png"), LOSSLESS_COMPRESSION_TYPE, 0.5f,
+                  "1_lossless_50.webp" },
+            new Object[] { loadImage("1.png"), LOSSLESS_COMPRESSION_TYPE, 1f, "1_lossless_100.webp" },
+            new Object[] { loadImage("2.png"), LOSSY_COMPRESSION_TYPE, 0.9f, "2_lossy_90.webp" },
+            new Object[] { loadImage("3.png"), LOSSY_COMPRESSION_TYPE, 0.9f, "3_lossy_90.webp" },
+            new Object[] { loadImage("4.png"), LOSSY_COMPRESSION_TYPE, 0.9f, "4_lossy_90.webp" },
+            new Object[] { loadImage("5.png"), LOSSY_COMPRESSION_TYPE, 0.9f, "5_lossy_90.webp" } };
+   }
+
+   /**
+    * Test method tests {@link WebPWriter} with image resize options.
+    *
+    * @throws IOException
+    *            the test fails.
+    */
+   @Test(dataProvider = "createImagesWithScaleOptions", enabled = true)
+   public void testImageWriterScale(final RenderedImage image, final float xScale,
+         final float yScale, final String outputName) throws IOException {
+      final String extension = outputName.substring(outputName.lastIndexOf(".") + 1);
+
+      // Scale the image.
+      final RenderedOp scaledImage = ScaleDescriptor.create(image, xScale, yScale, 0f, 0f,
+            Interpolation.getInstance(Interpolation.INTERP_BICUBIC_2), null);
+
+      // get writer
+      final ImageWriter imgWriter = ImageIO.getImageWritersByFormatName(extension).next();
+      final ImageWriteParam imgWriteParams = new WebPWriteParam(null);
+      final String testName = "ScaleOptions";
+      final File file = createOutputFile(testName, outputName);
+      final ImageOutputStream imageOutputStream = ImageIO.createImageOutputStream(file);
+      try {
+         imgWriter.setOutput(imageOutputStream);
+         imgWriter.write(null, new IIOImage(scaledImage, null, null), imgWriteParams);
+         final int length = (int) imageOutputStream.length();
+         assertTrue(length > 0);
+      } finally {
+         try {
+            imageOutputStream.close();
+         } catch (final IOException e) {
+         }
+      }
+   }
+
+   /**
+    * The data provider for
+    * {@link #testImageWriterScale(RenderedImage, float, float, String)}.
+    *
+    * @return the test images and options.
+    * @throws IOException
+    *            if unable to read one of the test images.
+    */
+   @DataProvider
+   public Object[][] createImagesWithScaleOptions() throws IOException {
+      return new Object[][] { new Object[] { loadImage("1.png"), 1, 2, "1_1x2.webp" },
+            new Object[] { loadImage("2.png"), 2, 1, "2_2x1.webp" },
+            new Object[] { loadImage("3.png"), 1, 5, "3_1x5.webp" },
+            new Object[] { loadImage("4.png"), 5, 1, "4_5x1.webp" },
+            new Object[] { loadImage("5.png"), 5, 5, "5_5x5.webp" },
+            new Object[] { loadImage("1.png"), 1, .5f, "1_1x.5.webp" },
+            new Object[] { loadImage("2.png"), .5f, 1, "2_.5x1.webp" },
+            new Object[] { loadImage("3.png"), 1, .2f, "3_1x.2.webp" },
+            new Object[] { loadImage("4.png"), .2f, 1, "4_.2x1.webp" },
+            new Object[] { loadImage("5.png"), .2f, .2f, "5_.2x.2.webp" } };
+   }
+
+   /**
+    * Loads the image with the given name.
+    *
+    * @param name
+    *           the name of the image to load.
+    * @return the requested image.
+    * @throws IOException
+    *            if unable to read the image.
+    */
+   private RenderedImage loadImage(final String name) throws IOException {
+      final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+      return ImageIO.read(classLoader.getResource(name));
+   }
+
+   /**
     * Creates the output file in the specified test directory.
-    * 
+    *
     * @param testName
     *           the name of the test to use as the output directory name.
     * @param outputName
@@ -115,31 +211,7 @@ public class WebPWriterTest {
    private File createOutputFile(final String testName, final String outputName) {
       final File parentDirectory = new File("target" + File.separator + testName);
       parentDirectory.mkdirs();
-      File file = new File(parentDirectory, outputName);
+      final File file = new File(parentDirectory, outputName);
       return file;
-   }
-
-   /**
-    * The data provider for all test images.
-    *
-    * @return the test {@link BufferedImage}s.
-    * @throws IOException
-    *            if unable to read one of the test images.
-    */
-   @DataProvider
-   public Object[][] testImageOptions() throws IOException {
-      final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-      return new Object[][] {
-            new Object[] { ImageIO.read(classLoader.getResource("1.png")), LOSSY_COMPRESSION_TYPE, 0f, "1_lossy_0.webp" },
-            new Object[] { ImageIO.read(classLoader.getResource("1.png")), LOSSY_COMPRESSION_TYPE, 0.5f, "1_lossy_50.webp" },
-            new Object[] { ImageIO.read(classLoader.getResource("1.png")), LOSSY_COMPRESSION_TYPE, 0.9f, "1_lossy_90.webp" },
-            new Object[] { ImageIO.read(classLoader.getResource("1.png")), LOSSY_COMPRESSION_TYPE, 1f, "1_lossy_100.webp" },
-            new Object[] { ImageIO.read(classLoader.getResource("1.png")), LOSSLESS_COMPRESSION_TYPE, 0f, "1_lossless_0.webp" },
-            new Object[] { ImageIO.read(classLoader.getResource("1.png")), LOSSLESS_COMPRESSION_TYPE, 0.5f, "1_lossless_50.webp" },
-            new Object[] { ImageIO.read(classLoader.getResource("1.png")), LOSSLESS_COMPRESSION_TYPE, 1f, "1_lossless_100.webp" },
-            new Object[] { ImageIO.read(classLoader.getResource("2.png")), LOSSY_COMPRESSION_TYPE, 0.9f, "2_lossy_90.webp" },
-            new Object[] { ImageIO.read(classLoader.getResource("3.png")), LOSSY_COMPRESSION_TYPE, 0.9f, "3_lossy_90.webp" },
-            new Object[] { ImageIO.read(classLoader.getResource("4.png")), LOSSY_COMPRESSION_TYPE, 0.9f, "4_lossy_90.webp" },
-            new Object[] { ImageIO.read(classLoader.getResource("5.png")), LOSSY_COMPRESSION_TYPE, 0.9f, "5_lossy_90.webp" } };
    }
 }
